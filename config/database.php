9ea27d93df6c137a3037aceee6f8e722d4deadff
theroadisnear <2012042055@ust-ics.mygbiz.com>
2015-07-20 13:46:43 +0800

    Open shift

--- config/database.php
+++ config/database.php
@@ -54,10 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'TimeKeepingDb'),
-            'username'  => env('DB_USERNAME', ''),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST')),
+            'database'  => env('DB_DATABASE', getenv('OPENSHIFT_APP_NAME')),
+            'username'  => env('DB_USERNAME', getenv('OPENSHIFT_MYSQL_DB_USERNAME')),
+            'password'  => env('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD')),
+            'port'      => env('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT')),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
